caf9c2a6e9dece2e819706dfa100678021430f39
Bukhari bin mahmoud Kibuka <93518580+bumahkib7@users.noreply.github.com>
2023-02-14 15:50:17 +0100

    :art: feat: adding angular

---
+++ frontend_angular/src/app/main-drawer/main-drawer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-drawer',
+  templateUrl: './main-drawer.component.html',
+  styleUrls: ['./main-drawer.component.scss']
+})
+export class MainDrawerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
